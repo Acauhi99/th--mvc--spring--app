46638e6f0db9bdfc90cc17e1735482b362fe6634
Mateus Acauhi <mateusacauhi@bluems.com>
2025-05-17 15:45:07 -0300

    chore: corrigir a exibicao das paginas

--- src/main/java/acauhi/mvc/spring/config/SecurityConfig.java
+++ src/main/java/acauhi/mvc/spring/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
+            .requestMatchers("/css/**", "/js/**", "/images/**", "/styles.css").permitAll()
             .requestMatchers("/", "/login", "/register").permitAll()
             .requestMatchers("/users").hasAuthority("ROLE_ADMIN")
             .requestMatchers("/users/create").hasAuthority("ROLE_ADMIN")
